0d389aae10ac70996b591f947a41546972b46ce3
Alex Cullen <alex_cullen@hms.harvard.edu>
2021-08-27 11:55:22 -0400

    Revert "If user declares their own requirements, don't set subscribe to undef."
    This reverts commit 8591aed9e8ba0a24a64914f8c302ffc4a8178f6d.

--- manifests/requirements.pp
+++ manifests/requirements.pp
@@ -88,8 +88,11 @@ define python::requirements (
       replace => false,
       content => '# Puppet will install and/or update pip packages listed here',
     }
+
+    $local_subscribe = File[$requirements]
+  } else {
+    $local_subscribe = undef
   }
-  $local_subscribe = File[$requirements]
 
   exec { "python_requirements${name}":
     provider    => shell,
